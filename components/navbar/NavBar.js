49fe38961b81a32231790e57ff69a01ae1379c97
edwinsanjo <edwinsanjo09@gmail.com>
2022-05-20 20:36:10 +0530

    afgav

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -1,18 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function NavBar() {
     return (
         <nav className='flex items-center justify-center lg:justify-between pt-4  lg:px-32 lg:py-6'>
             <div className=''>
-                <a href="/" className='text-lg font-bold text-white'>Edwin Sanjo Soji</a>
+                <Link href="/" className='text-lg font-bold text-white'>Edwin Sanjo Soji</Link>
             </div>
             <div className='hidden lg:block'>
                 <ul className="text-white md:flex space-x-7">
-                    <li className="font-semibold font-sm"><a href="">Home</a></li>
-                    <li className="font-semibold font-sm"><a href="">About</a></li>
-                    <li className="font-semibold font-sm"><a href=''>Projects</a></li>
-                    <li className="font-semibold font-sm"><a href="">Contact</a></li>
+                    <li className="font-semibold font-sm"><Link href="">Home</Link></li>
+                    <li className="font-semibold font-sm"><Link href="">About</Link></li>
+                    <li className="font-semibold font-sm"><Link href=''>Projects</Link></li>
+                    <li className="font-semibold font-sm"><Link href="">Contact</Link></li>
                 </ul>
             </div>
         </nav >
